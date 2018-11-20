1859452611598e31bf8e7656e98c41abd2aa3987
Jni <jni.viens@gmail.com>
2018-11-20 01:35:05 -0500

    Allows missing app parameters

--- src/main/java/ca/ulaval/glo4002/cart/context/ApplicationContext.java
+++ src/main/java/ca/ulaval/glo4002/cart/context/ApplicationContext.java
@@ -1,20 +1,27 @@
 package ca.ulaval.glo4002.cart.context;
 
 public class ApplicationContext {
+
+    public static final String STORE_PARAMETER = "store";
+    public static final String PROMO_PARAMETER = "promo";
+    public static final String MODE_PARAMETER = "mode";
+
     public void apply() {
-        if (System.getProperty("store").equalsIgnoreCase("xml")) {
+        if (hasParameterValue(STORE_PARAMETER, "xml")) {
             new XmlPersistenceContext().apply();
-        } else {
+        } else if (hasParameterValue(STORE_PARAMETER, "memory")) {
             new InMemoryPersistenceContext().apply();
+        } else {
+            throw new RuntimeException("No story parameter provided. Requires -D" + STORE_PARAMETER + "=xml|memory");
         }
 
-        if (System.getProperty("promo").equalsIgnoreCase("true")) {
+        if (hasParameterValue(PROMO_PARAMETER, "true")) {
             new PromoContext().apply();
         } else {
             new NoPromoContext().apply();
         }
 
-        if (System.getProperty("mode").equalsIgnoreCase("demo")) {
+        if (hasParameterValue(MODE_PARAMETER, "demo")) {
             new DemoPrefillContext().apply();
         }
     }
